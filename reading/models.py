29a7e61862ee0c8214049802c78cfb4dabfdd6aa
Raju <mhraju069@gmail.com>
2026-04-11 02:58:00 +0600

    update: ReadingQuestion

--- reading/models.py
+++ reading/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 import uuid
 # Create your models here.
 
@@ -66,7 +66,7 @@ class QuestionSet(models.Model):
 
 class ReadingResult(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=True, blank=True)
     set = models.ForeignKey(QuestionSet, on_delete=models.CASCADE)
     answers = models.JSONField(null=True, blank=True)
